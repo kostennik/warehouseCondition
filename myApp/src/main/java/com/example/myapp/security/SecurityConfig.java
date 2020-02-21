4d2f8ef3d03456d4d7409a6876f4c7860b44d86a
Kostennik <kostennik@gmail.com>
2020-02-21 21:56:25 +0100

    added simple authorization support using Spring-Security (in Memory method)

---
+++ myApp/src/main/java/com/example/myapp/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.myapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")  //we can do as here
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("user2")
+                .password(encoder.encode("password2")) //or we can do as here
+                .authorities("ROLE_USER");
+    }
+}
